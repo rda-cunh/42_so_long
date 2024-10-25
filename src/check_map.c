e5d94299f11703ee1cf51f243311f7f0b55f38f1
rda-cunh <ricard.mendes@gmail.com>
2024-10-25 11:48:14 +0100

    updated sprites

--- src/check_map.c
+++ src/check_map.c
@@ -6,7 +6,7 @@
 /*   By: rda-cunh <rda-cunh@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 23:06:42 by rda-cunh          #+#    #+#             */
-/*   Updated: 2024/10/25 01:48:37 by rda-cunh         ###   ########.fr       */
+/*   Updated: 2024/10/25 10:28:55 by rda-cunh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,5 @@ void	check_map(t_game *so_long)
 	check_walls(so_long);
 	count_map_objects(so_long);
 	verify_map_objects(so_long);
-    check_paths(so_long);
+	check_paths(so_long);
 }
